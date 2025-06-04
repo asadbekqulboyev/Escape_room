5fc32c7e257387bed1891643da1b025176cc896b
asadbek <asadbekqulboyev@gmail.com>
2025-06-04 17:34:11 +0500

    finish desctop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,105 @@
 $(document).ready(function () {
-  const $scrollable = $("#scrollable");
-  const $thumb = $("#thumb");
+  new Swiper(".testimonial__slider", {
+    slidesPerView: 4.1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4.4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 1,
+      },
+    },
+  });
+  const $container = $(".works__content"); // Scroll qilingan div
+  const $thumb = $(".scrollbar_thumb");
+  const $scrollbar = $(".scrollbar");
+  const $items = $(".works__content__item");
 
-  $scrollable.on("scroll", function () {
-    const scrollTop = $(this).scrollTop();
-    const maxScroll = this.scrollHeight - this.clientHeight;
-    const trackHeight = this.clientHeight * 0.8; // .line height
-    const thumbHeight = 60;
-    const maxThumbTop = trackHeight - thumbHeight;
+  function updateThumb() {
+    const containerHeight = $container.height();
+    const contentHeight = $container[0].scrollHeight;
+    const scrollTop = $container.scrollTop();
 
-    const thumbTop = (scrollTop / maxScroll) * maxThumbTop;
-    $thumb.css("top", `calc(10% + ${thumbTop}px)`);
+    const thumbHeight = Math.max(
+      (containerHeight / contentHeight) * containerHeight,
+      50
+    ); 
+    const thumbTop = (scrollTop / contentHeight) * containerHeight;
+
+    $thumb.css({
+      height: thumbHeight + "px",
+      top: thumbTop + "px",
+    });
+  }
+
+  function updateActiveItems() {
+    const containerTop = $container.scrollTop();
+    const containerHeight = $container.height();
+
+    $items.each(function () {
+      const $item = $(this);
+      const itemTop = $item.position().top;
+      const itemHeight = $item.outerHeight();
+
+      if (itemTop + itemHeight > 20 && itemTop < containerHeight - 20) {
+        $item.addClass("active");
+      } else {
+        $item.removeClass("active");
+      }
+    });
+  }
+
+  function onScroll() {
+    updateThumb();
+    updateActiveItems();
+  }
+
+  // Scroll harakati
+  $container.on("scroll", onScroll);
+
+  // Boshlanishida
+  onScroll();
+
+  // Thumbni drag qilish
+  let isDragging = false;
+  let startY = 0;
+  let startScrollTop = 0;
+
+  $thumb.on("mousedown", function (e) {
+    e.preventDefault();
+    isDragging = true;
+    startY = e.pageY;
+    startScrollTop = $container.scrollTop();
+    $("body").addClass("no-select");
+  });
+
+  $(document).on("mousemove", function (e) {
+    if (!isDragging) return;
+
+    const deltaY = e.pageY - startY;
+    const containerHeight = $container.height();
+    const contentHeight = $container[0].scrollHeight;
+    const scrollableHeight = contentHeight - containerHeight;
+
+    const thumbMoveRatio = scrollableHeight / containerHeight;
+    $container.scrollTop(startScrollTop + deltaY * thumbMoveRatio);
+  });
+
+  $(document).on("mouseup", function () {
+    isDragging = false;
+    $("body").removeClass("no-select");
   });
 });
